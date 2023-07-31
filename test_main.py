25b667f31c1bde6a0cccf7a0102a97957a18afae
poonamadhikari96 <140783203+poonamadhikari96@users.noreply.github.com>
2023-07-31 15:58:02 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,1 +1,26 @@
+# DO NOT MODIFY THE CODE IN THIS FILE
+
+# Tests for Problem 1
+# Simple Pay Calculator
+
+import os.path
+import sys
+from main import main
+
+def test_problem_1():
+
+    try:
+        exists = os.path.exists("test_main.py")
+        assert exists == True
+    except:
+        sys.exit()
+
+    set_keyboard_input(['poonam'])
+    main()
+    output = get_display_output()
+
+    assert output == [
+            "Input user name: ",
+            "\npoonam" 
+    ]
 
